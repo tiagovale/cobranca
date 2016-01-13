42bf3e29d3fed95cc86899c7cbf629fa0caab5d9
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-01-13 20:51:30 -0200

    Showed the success messagem after saving a title

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
@@ -11,7 +12,7 @@ import com.algaworks.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
 
@@ -19,12 +20,14 @@ public class TituloController {
 	public String novo() {
 		return "CadastroTitulo";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		
+	public ModelAndView salvar(Titulo titulo) {
+
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem","Titulo salvo com suceso!");
+		return mv;
 	}
-	
+
 }
