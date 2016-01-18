3cc361f3230748280aa0c50f377f60adf94d2dec
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-01-17 23:45:54 -0200

    Implemented the delete method for the title. Added the modal to ask the user if he wants to exclud the object.

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -59,6 +59,14 @@ public class TituloController {
 		return mv;
 	}
 
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		titulos.delete(codigo);
+		
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
+	}
+
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 
