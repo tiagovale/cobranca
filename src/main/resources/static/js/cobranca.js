850d8a260dd717bb17c91e84f3cd475d3b89fb49
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-01-19 19:00:55 -0200

    Included the jquery mask money library for formatting number fields and configured the field value.

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -19,4 +19,9 @@ $('#confirmacaoExclusaoModal').on(
 
 $(function() {
 	$('[rel="tooltip"]').tooltip();
+	$('.js-currency').maskMoney({
+		decimal : ',',
+		thousands : '.',
+		allowZero : true
+	});
 });
